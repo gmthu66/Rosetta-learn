10c4ce48fa6c94288012b8adecb0bfca92f1b968
Tileli Amimeur <Leli@Tilelis-MacBook-Pro.local>
2017-05-12 14:35:43 -0700

    changing dir names to appease setup.py

--- Rosetta-learn/setup.py
+++ Rosetta-learn/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='Rosetta-learn',
+setup(name='Rosetta_learn',
       version='0.1',
       description='Recommender system for protein sequence design and optimization using Rosetta',
       url='https://github.com/tamimeur/Rosetta-learn.git',
       author='Tileli Amimeur',
       author_email='tamimeur@gmail.com',
       license='MIT',
-      packages=['Rosetta-learn'],
+      packages=['Rosetta_learn'],
       test_suite='nose.collector',
       tests_require=['nose'],
       install_requires=[
@@ -16,6 +16,6 @@ setup(name='Rosetta-learn',
       zip_safe=False,
       entry_points={
         'console_scripts': [
-            'Rosetta-learn = Rosetta-learn.cli:main',
+            'Rosetta-learn = Rosetta_learn.cli:main',
         ],
       })
